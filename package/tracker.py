d5c0361eb10c469aeab97dcbb784c90200a47c10
Suhan Park <44104676+suhanpark@users.noreply.github.com>
2021-04-18 04:07:49 -0400

    update

---
+++ package/tracker.py
@@ -0,0 +1,92 @@
+import phonenumbers as pn
+from phonenumbers import geocoder as gc
+from phonenumbers import carrier as car
+from phonenumbers import timezone as tz
+import requests
+import json
+
+
+def find_loc(number):
+	ch_number = pn.parse(number, "CH")
+	location = gc.description_for_number(ch_number, "en")
+	return location
+
+
+def isolate_nums(number):
+	num = ""
+	for i in number.strip():
+		if i.isnumeric():
+			num += i
+	return num[1:]
+
+
+def getCarrier_US(number):
+	url = 'https://api.telnyx.com/v1/phone_number/1' + number
+	html = requests.get(url).text
+	data = json.loads(html)
+	data = data["carrier"]
+	carrier = data["name"]
+	return carrier
+
+
+def find_carrier(number):
+	carrier = ""
+	country_check = str(pn.parse(number))
+	if "Country Code: 1 " in country_check:
+		num = isolate_nums(number)
+		carrier = getCarrier_US(num)
+	else:
+		service_number = pn.parse(number, "RO")
+		carrier = car.name_for_number(service_number, "en")
+	return carrier
+
+
+def phone_number_info(number):
+	pnum = pn.parse(number)
+	location = find_loc(number)
+	carrier = find_carrier(number)
+	time_zone = tz.time_zones_for_number(pnum)
+	validity = pn.is_valid_number(pnum)
+	possiblity = pn.is_possible_number(pnum)
+	L = [pnum, location, carrier, time_zone, validity, possiblity]
+	return L
+
+
+if __name__ == '__main__':
+	number = str(input("Insert your phone number with the country code/number\n"
+	               "> "))
+
+	try:
+		info = phone_number_info(number)
+
+		# number
+		print("\nPhone Number:")
+		print(info[0])
+
+		# location
+		print("\nLocation:", info[1])
+
+		# carrier
+		print("\nCarrier or Service Provider:", info[2])
+
+		# time zone
+		print("\nTime Zone:")
+		for zone in info[3]:
+			print(zone, "\n")
+
+		# validity
+		if info[4]:
+			print("The phone number is valid")
+		else:
+			print("The phone number is invalid")
+
+		# possibility
+		if info[5]:
+			print("The phone number is possible\n")
+		else:
+			print("The phone number is impossible\n")
+
+		print("Processing Completed.")
+	except:
+		raise
+		print("Error Occurred. Please check your input.")
